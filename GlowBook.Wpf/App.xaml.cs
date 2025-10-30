643f821bf401b0b7fa15c043c1e1e19cdf0ec2bb
milatxx <146560367+milatxx@users.noreply.github.com>
2025-10-30 14:20:40 +0100

    WPF app setup: styles/theme in App.xaml + DI/SQLite in App.xaml.cs

--- GlowBook.Wpf/App.xaml.cs
+++ GlowBook.Wpf/App.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using GlowBook.Model.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using System;
+using GlowBook.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace GlowBook.Wpf
 {
@@ -9,6 +14,43 @@ namespace GlowBook.Wpf
     /// </summary>
     public partial class App : Application
     {
-    }
+        public static IHost HostApp { get; private set; } = null!;
+        public static IServiceProvider Services => HostApp.Services;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            HostApp = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddDbContext<AppDbContext>(opt =>
+                        opt.UseSqlite("Data Source=glowbook.db"));
+
+                    services.AddIdentityCore<ApplicationUser>(o =>
+                    {
+                        o.Password.RequireDigit = false;
+                        o.Password.RequireNonAlphanumeric = false;
+                        o.Password.RequiredLength = 6;
+                    })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<AppDbContext>();
+
+                    services.AddScoped<UserManager<ApplicationUser>>();
+                    services.AddScoped<SignInManager<ApplicationUser>>();
+                })
+                .Build();
+
+            // da DB exists/migrated
+            using var scope = Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            HostApp.Dispose();
+            base.OnExit(e);
+        }
+    }
 }
