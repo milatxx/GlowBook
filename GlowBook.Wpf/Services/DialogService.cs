8e5788f4e5a3d7ca03dd8c1a9d14e6ba3215336d
milatxx <146560367+milatxx@users.noreply.github.com>
2025-10-30 14:54:06 +0100

    feat(wpf): add RelayComand and DialogService (helpers + dialogs)

---
+++ GlowBook.Wpf/Services/DialogService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Windows;
+
+namespace GlowBook.Wpf.Services
+{
+    public static class DialogService
+    {
+        public static void Info(string msg) => MessageBox.Show(msg, "GlowBook", MessageBoxButton.OK, MessageBoxImage.Information);
+        public static void Warn(string msg) => MessageBox.Show(msg, "GlowBook", MessageBoxButton.OK, MessageBoxImage.Warning);
+        public static void Error(string msg) => MessageBox.Show(msg, "GlowBook", MessageBoxButton.OK, MessageBoxImage.Error);
+        public static bool Confirm(string msg) => MessageBox.Show(msg, "Bevestigen", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
+    }
+}
