a5d1f33ce2f69d465f62f792fcfcbb7bccc8778d
milatxx <146560367+milatxx@users.noreply.github.com>
2025-10-31 17:39:34 +0100

    feat(ui): add babypink SplashScreen and auto navigate to login

---
+++ GlowBook.Wpf/Views/SplashScreen.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GlowBook.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            Loaded += async (_, __) =>
+            {
+                await Task.Delay(1500);
+                new LoginWindow().Show();
+                Close();
+            };
+        }
+    }
+}
